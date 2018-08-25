7f9b9f78d989d18c2a7389a123bc234092dc2c0a
Vibhutha Kumarage <vkartz@gmail.com>
2018-08-25 17:57:16 +0530

    A

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -6,9 +6,10 @@
     <br>
     <input type="password" name="password" v-model="password" placeholder="Password" />
     <br>
+    <div class="error" v-html="error" />
+    <br>
     <button @click="register"> Register </button>
   </div>
-
 </template>
 
 <script>
@@ -16,21 +17,30 @@ import authenticationService from '@/services/authenticationService'
 export default {
   data () {
     return {
-      email: 'abcd',
-      password: '123445'
+      email: '',
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await authenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await authenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
 </script>
 
 <style scoped>
+
+.error {
+  color: red;
+}
+
 </style>
