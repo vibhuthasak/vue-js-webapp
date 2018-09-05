e9c9dfd63015310e3b44ed5e00779cafe5d47b3c
Vibhutha Kumarage <vkartz@gmail.com>
2018-09-05 23:31:13 +0530

    Song component added

---
+++ client/src/components/Songs.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-layout row>
+    <v-flex xs6 offset-xs3>
+      <panel title="Songs">
+        <slot>
+          <div v-for="song in songs" :key="song.id">
+            {{song.title}} -
+            {{song.artist}} -
+            {{song.album}}
+          </div>
+        </slot>
+      </panel>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import SongService from '@/services/SongService'
+import Panel from '@/components/Panel'
+export default {
+  components: {
+    Panel
+  },
+  data () {
+    return {
+      songs: null
+    }
+  },
+  async mounted () {
+    var songsRes = await SongService.index()
+    console.log('Songs', songsRes)
+    this.songs = songsRes.data
+  }
+}
+</script>
+
+<style scoped>
+</style>
